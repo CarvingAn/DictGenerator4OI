70e36886ffe459f8ac4666f450b11776b274ac4f
CarvingAn <205490595+CarvingAn@users.noreply.github.com>
2025-08-23 19:56:50 +0800

    #05 Add Error Handling
    Add Error Handling codes to Callpy.py that can make the program not panic when there's an unreadable file or the python files itself has an error.

--- utils/Callpy.py
+++ utils/Callpy.py
@@ -1,8 +1,14 @@
 import sys,_io
+from PySide6.QtWidgets import QMessageBox
+
+
 class CallPy():
     def __init__(self,path):
-        with open(path,'r') as f:
-            self.code=f.read()
+        try:
+            with open(path,'r') as f:
+                self.code=f.read()
+        except Exception as e:
+            QMessageBox.critical(None,"Error",f"An error occurred while reading files: {e}")
     def __call__(self,data):
         a=sys.stdin,sys.stdout
         sys.stdin = _io.StringIO(str(data)+"\n")
@@ -10,6 +16,10 @@ class CallPy():
         try:
             exec(self.code)
             result = sys.stdout.getvalue()
+        except ModuleNotFoundError:
+            QMessageBox.critical(None,"Error","Please do not use third-party libs.\n{e}")
+        except Exception as e:
+            QMessageBox.critical(None,"Error",f"An error occurred while executing code: {e}")
         finally:
             sys.stdin,sys.stdout=a
         return result
